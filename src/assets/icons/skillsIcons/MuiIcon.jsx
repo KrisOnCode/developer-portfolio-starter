c3e1c21dd901b3a248c26a03c066ab7b74ee86c0
Kris Paschal <krispaschal@gmail.com>
2022-09-30 12:04:18 -0500

    initial commit

---
+++ src/assets/icons/skillsIcons/MuiIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MuiIcon() {
+    return (
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 17.3879V0L12 8.69396V14.4899L4 8.69396V20.286L0 17.3879Z" fill="#00B0FF"/>
+        <path d="M12 8.69396L24 0V17.3879L16 23.1838L12 20.286L20 14.4899V8.69396L12 14.4899V8.69396Z" fill="#0081CB"/>
+        <path d="M12 20.2859V26.0819L20 31.8778V26.0819L12 20.2859Z" fill="#00B0FF"/>
+        <path d="M20 31.8778L32 23.1838V11.5922L28 14.49V20.2861L20 26.082V31.8781V31.8778ZM28 8.69396V2.89788L32 0V5.79608L28 8.69396Z" fill="#0081CB"/>
+        </svg>
+    )
+}
